e4271df7f557e346e0c534679597b74dfc652a7b
harberwef <141514257+harberwef@users.noreply.github.com>
2023-10-01 02:21:13 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -71,8 +71,8 @@ def master_class(update: Update, context: CallbackContext):
 ✅Access a free-quality gold signal for two month
 ✅24/7 student guidance
 ✅A big giveaway at the end of the class
-✅Course duration: 3 month
-💸Payment: 500$\nFinish Your Payment Using One of the methods and send the screenshot to @xesniper9""",
+✅Course duration: 3 month\nFinish Your Payment Using One of the methods and send the screenshot to @xesniper9""",
+💸Payment: 500$
     query = update.callback_query
     keyboard = [
         [
@@ -130,7 +130,7 @@ def one_to_one(update: Update, context: CallbackContext):
 ✅A big giveaway at the end of the class
 ✅Get Xe Sniper Digital Certificate
 ✅Course duration: 1 month and 15 days
-💸Payment: 100$\10000 birr\nFinish Your Payment Using One of the methods and send the screenshot to @xesniper9""",
+💸Payment: 100$ or 10000 birr\nFinish Your Payment Using One of the methods and send the screenshot to @xesniper9""",
         reply_markup=InlineKeyboardMarkup(keyboard),
     )
 
